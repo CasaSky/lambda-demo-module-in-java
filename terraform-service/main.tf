ed44cb4179b45ce33d0faf4fb3ba2d53c10e56b6
Talal Tabia <mail@talaltabia.com>
2021-10-17 11:03:29 +0200

    add encryption using KMS

--- terraform-service/main.tf
+++ terraform-service/main.tf
@@ -5,8 +5,15 @@ provider "aws" {
 module "lambda_sqs" {
   source = "git@github.com:casasky/terraform-modules.git//lambda-sqs"
 
-  function_name = "lambda-demo-module"
-  filepath      = "${path.module}/../build/distributions/lambda-demo-module-in-java.zip"
-  handler       = "com.casasky.Handler"
-  runtime       = "java11"
+  function_name     = "lambda-demo-module"
+  filepath          = "${path.module}/../build/distributions/lambda-demo-module-in-java.zip"
+  handler           = "com.casasky.Handler"
+  runtime           = "java11"
+  kms_master_key_id = module.kms_for_sqs.kms_master_key_id
+}
+
+module "kms_for_sqs" {
+  source      = "git@github.com:casasky/terraform-modules.git//kms"
+  description = "KMS used for encrypting all SQS messages"
+  alias       = "sqs"
 }
