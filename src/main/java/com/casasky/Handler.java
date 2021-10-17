c8f506739d6109dba40976560f8fc30476012b57
Talal Tabia <mail@talaltabia.com>
2021-10-17 19:19:12 +0200

    fix logging

--- src/main/java/com/casasky/Handler.java
+++ src/main/java/com/casasky/Handler.java
@@ -6,13 +6,25 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
+import java.util.stream.Collectors;
+
 public class Handler implements RequestHandler<SQSEvent, String> {
 
-    private static final Logger logger = LogManager.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public String handleRequest(SQSEvent input, Context context) {
-        logger.info("Handling request...");
+        if (input.getRecords() == null) {
+            LOGGER.debug("No records found");
+
+            return "Failure";
+        }
+
+        LOGGER.debug("Handling request {}", input.getRecords().stream()
+                .map(SQSEvent.SQSMessage::getBody)
+                .collect(Collectors.toList()));
+
         return "Success";
     }
 }
